1a8e5bf4790dbc8e2e95f189e8dcab5cc73929a3
Ahmed Khaled <ahmmkhaled20@gmail.com>
2024-03-31 23:41:38 +0200

    setup gradle

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlinx.serialization)
-
+    id("co.touchlab.skie") version "0.6.2"
 }
 
 kotlin {
