188c71905d70a08d896f74013514ab04767a3282
Azizah <aziza.helmy35@gmail.com>
2024-03-31 12:27:01 +0200

    add checks

--- shared/src/commonMain/kotlin/data/source/remote/network/ApiService.kt
+++ shared/src/commonMain/kotlin/data/source/remote/network/ApiService.kt
@@ -13,13 +13,13 @@ import org.koin.core.component.KoinComponent
  */
 class ApiService(private val client: HttpClient) : IApiService, KoinComponent {
     override suspend fun getUsers(): RandomUserApiResponse {
-        return tryToExecute(client.get("?page=1&results=20&seed=abc"))
+        return tryToExecute(client.get("?page=1&results=20&seed=abc")) //todo: just 4 testing
     }
     private suspend inline fun <reified T> tryToExecute(response: HttpResponse): T {
         if (response.status.isSuccess()) {
             return response.body()
         } else {
-            when (response.status.value) {
+            when (response.status.value) { //todo: just for now
                 401 -> throw Exception(response.status.description)
                 500 -> throw Exception()
                 else -> throw Exception(response.status.description)
